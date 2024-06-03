3e966f3f90328e28292eda5aea60f6b1c4360d2d
Christine Berger <christinebergersd@gmail.com>
2024-06-03 13:59:30 -0500

    Added text summary

--- macros/compare_queries.sql
+++ macros/compare_queries.sql
@@ -1,8 +1,12 @@
-{% macro compare_queries(a_query, b_query, primary_key=None, summarize=true, return_all=false, limit=None) -%}
+{% macro compare_queries(a_query, b_query, primary_key=None, mode='summary', return_all=false, limit=None) -%}
   {{ return(adapter.dispatch('compare_queries', 'audit_helper')(a_query, b_query, primary_key, summarize, limit)) }}
 {%- endmacro %}
 
-{% macro default__compare_queries(a_query, b_query, primary_key=None, summarize=true, return_all=false, limit=None) %}
+{% macro default__compare_queries(a_query, b_query, primary_key=None, mode='summary', return_all=false, limit=None) %}
+-- Modes:
+    -- summary (same as before)
+    -- details (same as before)
+    -- text_summary (summary as text, not table)
 
 with a as (
 
@@ -66,9 +70,9 @@ all_records as (
 
 ),
 
-{%- if summarize %}
+{%- if mode in ('summary', 'text_summary') %}
 
-summary_stats as (
+summary_count as (
     select 
         in_a, 
         in_b, 
@@ -77,13 +81,35 @@ summary_stats as (
     group by 1, 2
 ),
 
-final as (
+summary_perc as (
     select
         *,
         round(100.0 * count / sum(count) over (), 2) as percent_of_total
     from summary_stats
     order by in_a desc, in_b desc
+),
+
+{%- if mode == 'summary' %}
+
+final as (select * from summary_perc)
+
+{%- else %}
+
+final as (
+    select
+        'Perfect matches: ' 
+        || coalesce(min(case when in_a and in_b then percent_of_total end), 0) || '% ('
+        || coalesce(min(case when in_a and in_b then count end), 0) || ')\n'
+        || '{{ a_alias }} diffs: '
+        || coalesce(min(case when in_a and not in_b then percent_of_total end), 0) || '% ('
+        || coalesce(min(case when in_a and not in_b then count end), 0) || ')\n'
+        || '{{ b_alias }} diffs: '
+        || coalesce(min(case when not in_a and in_b then percent_of_total end), 0) || '% ('
+        || coalesce(min(case when not in_a and in_b then count end), 0) || ')'
+        as text_summary
+    from summary_perc
 )
+{%- endif %}
 
 {%- else %}
 
